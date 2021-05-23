f16cab904e63daa4c8664f68fa41f6d25e23c074
DESKTOP-EU4I8H8\josea <jarapa01@gmail.com>
2021-05-23 17:46:49 +0200

    Pixar responsive webpage with an image animation with hover

---
+++ js/funciones.js
@@ -0,0 +1,21 @@
+$(document).ready( function(){
+
+    console.log("Documento Cargado")
+
+});
+
+
+$(".logo_burger").click(function(){
+    
+    
+     $(".page_white").toggleClass("menu_open");
+     
+});
+
+
+
+
+
+
+
+
